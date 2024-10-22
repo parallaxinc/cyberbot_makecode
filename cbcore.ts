13d3f94cf54dfa9db5a63eab4d0de2aac7f59828
Wierdowl <b.conner81@yahoo.com>
2024-10-21 17:42:25 -0700

    Update cbcore.ts

--- cbcore.ts
+++ cbcore.ts
@@ -88,47 +88,47 @@ enum Units {
 //% color=#1D75B5 weight=100 icon="\uf2db" block="cyber:bot"
 //% groups='["Basic Read/Write", "Sound", "Servos", "Sensors", "Other"]'
 //% blockGap=4
-namespace cyberbot{
+namespace cyberbot {
 
     // commands
-    export const HIGH          = 1
-    export const LOW           = 2
-    export const INPUT         = 3
-    export const TOGGLE        = 4
+    export const HIGH = 1
+    export const LOW = 2
+    export const INPUT = 3
+    export const TOGGLE = 4
     //const SETDIRS       = 5
     //const GETDIRS       = 6
     //const SETSTATES     = 7
     //const GETSTATES     = 8
     //const PAUSE         = 9
-    export const PULSIN        = 10
-    export const PULSOUT       = 11
-    export const COUNT         = 12
-    export const FREQOUT       = 13
-    export const RCTIME        = 16
+    export const PULSIN = 10
+    export const PULSOUT = 11
+    export const COUNT = 12
+    export const FREQOUT = 13
+    export const RCTIME = 16
     //const SHIFTIN       = 17
     //const SHIFTOUT      = 18
     //const SEROUT        = 19
     //const SERIN         = 20
-    export const SERVO_ANGLE   = 24
-    export const SERVO_SPEED   = 25
+    export const SERVO_ANGLE = 24
+    export const SERVO_SPEED = 25
     //const SERVO_SET     = 26
     export const SERVO_SETRAMP = 27
     export const SERVO_DISABLE = 28
     //const SERVO_DRIVE   = 34
-    export const PING_ECHO     = 29
-    export const SIRC          = 30
-    export const IR_DETECT     = 31
-    export const PWM_OUT       = 32
-    export const QTI_READ      = 33
+    export const PING_ECHO = 29
+    export const SIRC = 30
+    export const IR_DETECT = 31
+    export const PWM_OUT = 32
+    export const QTI_READ = 33
     //const HANDSHAKE     = 99
     //const PWR_LED_WARN  = 25
     //const PWR_BRN_DET   = 24
-    
+
     //const PWR_LED_WARN  = 15
     //const PWR_BRN_DET   = 14    
 
-    export const ADDRESS       = 93
-    
+    export const ADDRESS = 93
+
     let isConnected = false;
 
     let navLightIsOn = false;
@@ -138,19 +138,19 @@ namespace cyberbot{
 
     let leftServo = ServoPin.Pin18
     let rightServo = ServoPin.Pin19
-    
+
     let leftForwardSpeed = 75
     let rightForwardSpeed = -75
     let leftReverseSpeed = -75
     let rightReverseSpeed = 75
 
-    function connect(){
+    function connect() {
 
         while (true) {
             pins.digitalWritePin(DigitalPin.P8, 0)
             pins.digitalWritePin(DigitalPin.P8, 1)
             pause(10);
-            if (pins.i2cReadNumber(ADDRESS, NumberFormat.UInt16LE) !== 0){
+            if (pins.i2cReadNumber(ADDRESS, NumberFormat.UInt16LE) !== 0) {
                 //pins.digitalWritePin(DigitalPin.P8, 1)
                 pause(10);
                 pins.i2cWriteNumber(ADDRESS, 12, NumberFormat.UInt16LE);
@@ -162,44 +162,44 @@ namespace cyberbot{
     }
 
 
-    function botDisable(): void{
-            pins.setPull(DigitalPin.P8, PinPullMode.PullNone);
-            basic.pause(200);
-            control.reset();
+    function botDisable(): void {
+        pins.setPull(DigitalPin.P8, PinPullMode.PullNone);
+        basic.pause(200);
+        control.reset();
     }
 
-    export function sendCommand(pinA: number, pinB: number=null, cmd:number, s=0, d:number=null, f:number=null): void{
-        if (isConnected === false){
+    export function sendCommand(pinA: number, pinB: number = null, cmd: number, s = 0, d: number = null, f: number = null): void {
+        if (isConnected === false) {
             connect()
         }
 
         // build args and write
-        if (pinB == null){
+        if (pinB == null) {
             pinB = 33;
         }
         let args = Buffer.fromArray([1, pinA, pinB, s]);
-        if (d !== null){
+        if (d !== null) {
             let duration = pins.createBuffer(4)
             duration.setNumber(NumberFormat.Int32LE, 0, Math.round(d))
             args = Buffer.concat([args, duration]);
         }
-        if (f !== null){
+        if (f !== null) {
             let freq = pins.createBuffer(4)
             freq.setNumber(NumberFormat.Int32LE, 0, Math.round(f))
-            args = Buffer.concat([args,freq]);
-        }   
+            args = Buffer.concat([args, freq]);
+        }
         console.log(args.toHex())
         pins.i2cWriteBuffer(ADDRESS, args);
-        
+
         // build command and write
-        pins.i2cWriteBuffer(ADDRESS, Buffer.fromArray([0,cmd]));
+        pins.i2cWriteBuffer(ADDRESS, Buffer.fromArray([0, cmd]));
 
         // wait until prop is done
         let check = 1
-        while (check !== 0){
+        while (check !== 0) {
             pins.i2cWriteNumber(ADDRESS, 0, NumberFormat.UInt8LE);
             check = pins.i2cReadNumber(ADDRESS, NumberFormat.UInt8LE);
-        }   
+        }
     }
 
     function read_r(): number {
@@ -211,14 +211,14 @@ namespace cyberbot{
     //% block="%p write digital %s"
     //% group="Basic Read/Write"
     //% weight=100
-    export function writeDigital(pin: BotPin, state: State): void{
+    export function writeDigital(pin: BotPin, state: State): void {
         sendCommand(pin, null, state, 0, null, null);
     }
 
     //% block="%pin write analog %f"
     //% group="Basic Read/Write"
     //% weight=94
-    export function writeAnalog(pin: BotPin, f: number): void{
+    export function writeAnalog(pin: BotPin, f: number): void {
         sendCommand(pin, null, PWM_OUT, 0, f, null);
     }
 
@@ -228,8 +228,8 @@ namespace cyberbot{
     export function readDigital(pin: BotPin): number {
         sendCommand(pin, null, INPUT, 0, null, null)
         let result = read_r();
-        if (result === 1){return 1;}
-        else {return 0;}
+        if (result === 1) { return 1; }
+        else { return 0; }
     }
 
     //% block="%pin is high"
@@ -238,8 +238,8 @@ namespace cyberbot{
     export function readDigitalBoole(pin: BotPin): boolean {
         sendCommand(pin, null, INPUT, 0, null, null)
         let result = read_r();
-        if (result === 1){return true;}
-        else {return false;}
+        if (result === 1) { return true; }
+        else { return false; }
     }
 
     /**
@@ -368,14 +368,14 @@ namespace cyberbot{
     //% block="%pin pulse out %d"
     //% group="Other"
     //% weight=100
-    export function pulseOut(pin: BotPin, d: number): void{
+    export function pulseOut(pin: BotPin, d: number): void {
         sendCommand(pin, null, PULSOUT, 0, d, null);
     }
 
     //% block="%pin pulse in %s"
     //% group="Other"
     //% weight=98
-    export function pulseIn(pin: BotPin, s: number): number{
+    export function pulseIn(pin: BotPin, s: number): number {
         sendCommand(pin, null, PULSIN, s, null, null);
         return read_r();
     }
@@ -383,7 +383,7 @@ namespace cyberbot{
     //% block="%pin pulse count %d"
     //% group="Other"
     //% weight=96
-    export function pulseCount(pin: BotPin, d:number): number{
+    export function pulseCount(pin: BotPin, d: number): number {
         sendCommand(pin, null, COUNT, 0, d, null);
         return read_r();
     }
@@ -391,15 +391,15 @@ namespace cyberbot{
     //% block="%pin rc time %s %d %f"
     //% group="Other"
     //% weight=95
-    export function rcTime(pin: BotPin, s:number): number{
+    export function rcTime(pin: BotPin, s: number): number {
         sendCommand(pin, null, RCTIME, s, null, null);
         return read_r();
     }
 
     //% block
     //% group="Other"
-    export function detach(){
-        while (true){
+    export function detach() {
+        while (true) {
             readDigital(25);
         }
     }
@@ -656,5 +656,4 @@ namespace cyberbot{
         sendCommand(leftServo, null, SERVO_DISABLE, 0, null);
         sendCommand(rightServo, null, SERVO_DISABLE, 0, null);
     }
-
 }

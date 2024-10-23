614a3428cbe025ce5c86ed9638d2f3fc84c07609
Wierdowl <b.conner81@yahoo.com>
2024-10-22 17:17:36 -0700

    Update test.ts

--- test.ts
+++ test.ts
@@ -19,7 +19,7 @@ input.onButtonPressed(Button.B, function () {
     cyberbot.servoStop(ServoPin.Pin19)
 })
 
-// Read Pins 7-4 and store there binary states in a single value, then check the binary state or each bit starting at bit 3 and ending at bit 0, then store it in a variable and scroll the states across the display
+// Read Pins 7-4 and store their binary states in a single value, then check the binary state or each bit starting at bit 3 and ending at bit 0, then store it in a variable and scroll the states across the display
 let qti = 0
 let bits = ""
 basic.forever(function () {
@@ -31,7 +31,7 @@ basic.forever(function () {
     basic.showString(bits)
 })
 
-// Create a dictionary, add a new key value pair, save the value from it and display it while waiting 5 seconds, then remove the pair, make the dictionary a string, and send it to another microbit using the long string send
+// Create a dictionary, add a new key-value pair, save the value from it, and display it while waiting 5 seconds, then remove the pair, make the dictionary a string, and send it to another microbit using the long string send
 let dict = dictionary.createDictionary(
     ["one", "two"],
     [1, 2]
@@ -45,7 +45,7 @@ let _package = dictionary.dictionaryToString(dict)
 radio.sendLongString(_package)
     
 
-// Read the received long signal, change it to a dictionary, save the values from the dict in separate variables, display each input that was received for 5 seconds
+// Read the received long signal, change it to a dictionary, save the values from the dict in separate variables, and display each input that was received for 5 seconds
 let dict: { [key: string]: any; } = null
 let one = 0
 let two = 0
@@ -60,8 +60,8 @@ radio.onLongMessageReceived(function (rLongString) {
     basic.pause(5000)
 })
 
-// Display a request for an input of your favorite animal on a terminal, wait for the input to be received with a carriage return, enter a new line on the serial, set everything entered to complete upper case, display the inputed string on the same terminal showing it is all upper case.
+// Display a request for an input of your favorite animal on a terminal, wait for the input to be received with a carriage return, enter a new line on the serial, set everything entered to complete upper case, display the inputted string on the same terminal showing it is all upper case.
 serial.writeString("What's your favorite animal:")
 let str = text.setToUpper(serial.readUntil(serial.delimiters(Delimiters.CarriageReturn)))
-serial.NewLine()
+serial.newLine()
 serial.writeLine("Your favorite animal is " + str)
